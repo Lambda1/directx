810e62e0c9a9413f3494e6798488415a52069357
U-LAMBDA\break_000 <breakthrough.dodorara@gmail.com>
2020-03-29 14:42:59 +0900

    add init

---
+++ study/x11/tutorial/tutorial/tutorial/source/main.cpp
@@ -0,0 +1,11 @@
+#include <Windows.h>
+
+/*
+	_In_, _In_opt_: SAL
+		C++ ÇÃÉRÅ[Éhè·äQëŒçÙ
+*/
+
+int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ PSTR pScmdline, _In_ int iCmdshow)
+{
+	return 0;
+}
