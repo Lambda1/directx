b9ab163ecc47becc8511b0d6662a1a02317a32e4
lambda1_letsnote <breakthrough.dodorara@gmail.com>
2020-09-19 14:21:47 +0900

    ch4をch5に移植

---
+++ study/dx12/grimoire/chapter05/BasicVertexShader.hlsl
@@ -0,0 +1,14 @@
+struct Output
+{
+	float4 pos:POSITION;
+	float4 svpos:SV_POSITION;
+};
+
+Output BasicVS(float4 pos : POSITION)
+{
+	Output output;
+	output.pos = pos;
+	output.svpos = pos;
+	
+	return output;
+}
