108a9930f2b79b4711823f17fe57bd6ee85203c0
lambda1 <breakthrough.dodorara@gmail.com>
2021-03-13 16:37:50 +0900

    デバイス生成

--- mydx12/src/mydx12.cpp
+++ mydx12/src/mydx12.cpp
@@ -5,7 +5,6 @@ namespace mla
 	mydx12::mydx12() :
 		m_frame_buffer_count(2)
 	{
-		EnableDebug(true);
 	}
 
 	mydx12::~mydx12()
@@ -15,6 +14,19 @@ namespace mla
 
 	void mydx12::Init(HWND hwnd)
 	{
+		UINT dxgi_flags = 0;
+#if _DEBUG
+		// デバッグ初期化
+		EnableDebug(false);
+		dxgi_flags |= DXGI_CREATE_FACTORY_DEBUG;
+#endif
+		// DXGIFactory生成
+		Microsoft::WRL::ComPtr<IDXGIFactory3> factory;
+		CreateDXGIFactory2(dxgi_flags, IID_PPV_ARGS(factory.ReleaseAndGetAddressOf()));
+		auto adapter = SearchHardwareAdapter(L"NVIDIA", factory);
+		// D3D12デバイス生成
+		HRESULT create_device_result = D3D12CreateDevice(adapter.Get(), D3D_FEATURE_LEVEL_12_0, IID_PPV_ARGS(m_device.ReleaseAndGetAddressOf()));
+		ErrorCheck(create_device_result, m_device.Get(), L"Error: D3D12CreateDevice");
 	}
 
 	void mydx12::Terminate()
@@ -39,6 +51,29 @@ namespace mla
 	}
 
 	// private
+	// DX12対応ハードウェアアダプタ検索
+	Microsoft::WRL::ComPtr<IDXGIAdapter1> mydx12::SearchHardwareAdapter(const std::wstring &use_adapter_name, const Microsoft::WRL::ComPtr<IDXGIFactory3>& factory)
+	{
+		Microsoft::WRL::ComPtr<IDXGIAdapter1> adapter;
+		for (UINT adapter_index = {}; factory->EnumAdapters1(adapter_index, &adapter) != DXGI_ERROR_NOT_FOUND; ++adapter_index)
+		{
+			DXGI_ADAPTER_DESC1 desc{};
+			adapter->GetDesc1(&desc);
+			
+			// 取得するアダプタの確認
+			std::wstring adapter_name = desc.Description;
+			if (adapter_name.find(use_adapter_name) == std::wstring::npos) { continue; }
+			if ((desc.Flags & DXGI_ADAPTER_FLAG_SOFTWARE) == DXGI_ADAPTER_FLAG_SOFTWARE) { continue; }
+
+			// DX12対応の確認
+			Microsoft::WRL::ComPtr<ID3D12Device> device;
+			auto hr = D3D12CreateDevice(adapter.Get(), D3D_FEATURE_LEVEL_12_0, IID_PPV_ARGS(&device));
+			if (SUCCEEDED(hr) && device) { break; }
+			adapter = nullptr;
+		}
+		return adapter;
+	}
+
 	// デバッグ有効化
 	void mydx12::EnableDebug(const bool& enable_gbv)
 	{
--- mydx12/src/mydx12.hpp
+++ mydx12/src/mydx12.hpp
@@ -1,11 +1,15 @@
 #ifndef __MY_DX12_HPP__
 #define __MY_DX12_HPP__
 
+#include <string>
+
 #include <wrl.h>
 
 #include <d3d12.h>
+#include <dxgi1_6.h>
 
 #pragma comment(lib, "d3d12.lib")
+#pragma comment(lib, "dxgi.lib")
 
 namespace mla
 {
@@ -28,8 +32,23 @@ namespace mla
 
 	protected:
 		Microsoft::WRL::ComPtr<ID3D12Device> m_device;
-	private:
 
+		template<class T>
+		void ErrorCheck(const HRESULT& result, T* ptr, const std::wstring &msg)
+		{
+			if (FAILED(result) || !ptr)
+			{
+#if _DEBUG
+				OutputDebugStringW(msg.c_str());
+#endif
+				std::exit(EXIT_FAILURE);
+			}
+		}
+	private:
+		// 初期化関係
+		Microsoft::WRL::ComPtr<IDXGIAdapter1> SearchHardwareAdapter(const std::wstring &use_adapter_name, const Microsoft::WRL::ComPtr<IDXGIFactory3>& factory);
+
+		// デバッグ関係
 		void EnableDebug(const bool &enable_gbv);
 		void EnableDebugLayer(Microsoft::WRL::ComPtr<ID3D12Debug> &debug);
 		void EnableGPUBasedValidation(const Microsoft::WRL::ComPtr<ID3D12Debug> &debug);
